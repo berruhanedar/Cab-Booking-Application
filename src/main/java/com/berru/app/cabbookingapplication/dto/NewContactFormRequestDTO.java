7ed16daac6b37ade7d33b4e0f4aad597e0d56ea1
Berru Hanedar <berruhanedar@icloud.com>
2025-09-16 08:58:47 +0300

    add NewContactFormRequestDTO for creating new contact form requests

---
+++ src/main/java/com/berru/app/cabbookingapplication/dto/NewContactFormRequestDTO.java
@@ -0,0 +1,25 @@
+package com.berru.app.cabbookingapplication.dto;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class NewContactFormRequestDTO {
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotNull(message = "Phone number cannot be null")
+    @Digits(integer = 15, fraction = 0, message = "Phone number must be numeric and up to 15 digits")
+    private Long phone;
+
+    @NotNull(message = "Message cannot be null")
+    @Size(min = 5, max = 500, message = "Message must be between 5 and 500 characters")
+    private String message;
+
+}
