fee42aac04c6e0efea323d8eb0be85da0e09f0ee
Berru Hanedar <berruhanedar@icloud.com>
2025-09-16 14:48:51 +0300

    add ContactFormResponseDTO class

---
+++ src/main/java/com/berru/app/cabbookingapplication/dto/ContactFormResponseDTO.java
@@ -0,0 +1,14 @@
+package com.berru.app.cabbookingapplication.dto;
+
+public class ContactFormResponseDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private Long phone;
+
+    private String message;
+}
