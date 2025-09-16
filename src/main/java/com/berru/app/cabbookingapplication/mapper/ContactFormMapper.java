1b0f463770a26cf6449ad970b73816cad4c2247e
Berru Hanedar <berruhanedar@icloud.com>
2025-09-16 14:52:06 +0300

    refactor mapper for contact form and include response DTO conversion

--- src/main/java/com/berru/app/cabbookingapplication/mapper/ContactFormMapper.java
+++ src/main/java/com/berru/app/cabbookingapplication/mapper/ContactFormMapper.java
@@ -1,12 +1,15 @@
 package com.berru.app.cabbookingapplication.mapper;
 
+import com.berru.app.cabbookingapplication.dto.ContactFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewContactFormRequestDTO;
 import com.berru.app.cabbookingapplication.entity.ContactForm;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface BookingMapper {
+public interface ContactFormMapper {
 
     ContactForm toContactForm(NewContactFormRequestDTO newContactFormRequestDTO);
+
+    ContactFormResponseDTO toContactFormResponseDTO(ContactForm contactForm);
 
 }
