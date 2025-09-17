4f1c61fdb0347a305f944619dcdb66d590dfd646
Berru Hanedar <berruhanedar@icloud.com>
2025-09-17 14:31:55 +0300

    annotate NewContactFormRequestDTO with @Data for Lombok

--- src/main/java/com/berru/app/cabbookingapplication/dto/NewContactFormRequestDTO.java
+++ src/main/java/com/berru/app/cabbookingapplication/dto/NewContactFormRequestDTO.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class NewContactFormRequestDTO {
     @NotNull(message = "Name cannot be null")
     @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
