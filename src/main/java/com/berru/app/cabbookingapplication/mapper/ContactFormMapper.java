8d4f49b7b5c27269e4571fef4bbac89a7cc311a4
Berru Hanedar <berruhanedar@icloud.com>
2025-09-19 15:46:00 +0300

    add update mapping method in ContactFormMapper

--- src/main/java/com/berru/app/cabbookingapplication/mapper/ContactFormMapper.java
+++ src/main/java/com/berru/app/cabbookingapplication/mapper/ContactFormMapper.java
@@ -2,8 +2,10 @@ package com.berru.app.cabbookingapplication.mapper;
 
 import com.berru.app.cabbookingapplication.dto.ContactFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewContactFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.UpdateContactFormRequestDTO;
 import com.berru.app.cabbookingapplication.entity.ContactForm;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ContactFormMapper {
@@ -11,5 +13,7 @@ public interface ContactFormMapper {
     ContactForm toContactForm(NewContactFormRequestDTO newContactFormRequestDTO);
 
     ContactFormResponseDTO toContactFormResponseDTO(ContactForm contactForm);
+
+    void updateContactFormFromDTO(UpdateContactFormRequestDTO updateContactFormRequestDTO,@MappingTarget ContactForm contactForm);
 
 }
