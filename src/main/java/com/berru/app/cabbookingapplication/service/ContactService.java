2557095f9dd8b3d4285ffecab0f344b2f5f1c446
Berru Hanedar <berruhanedar@icloud.com>
2025-09-19 14:55:13 +0300

    define ContactService interface for managing contact forms and pagination

--- src/main/java/com/berru/app/cabbookingapplication/service/ContactService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/ContactService.java
@@ -2,7 +2,18 @@ package com.berru.app.cabbookingapplication.service;
 
 import com.berru.app.cabbookingapplication.dto.ContactFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewContactFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
+import com.berru.app.cabbookingapplication.dto.UpdateContactFormRequestDTO;
 
 public interface ContactService {
     ContactFormResponseDTO saveContactForm(NewContactFormRequestDTO newContactFormRequestDTO);
+
+    ContactFormResponseDTO getContactFormById(Integer id);
+
+    ContactFormResponseDTO updateContactForm(Integer id, UpdateContactFormRequestDTO updateContactFormRequestDTO);
+
+    void deleteContactFormById(Integer id);
+
+    PaginationResponse<ContactFormResponseDTO> listPaginated(int pageNo, int size);
+
 }
