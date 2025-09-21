7a6d1dec3266b6dba601745929eb3fbea60a9391
Berru Hanedar <berruhanedar@icloud.com>
2025-09-21 17:09:48 +0300

    add update mapping for BookingForm

--- src/main/java/com/berru/app/cabbookingapplication/mapper/BookingFormMapper.java
+++ src/main/java/com/berru/app/cabbookingapplication/mapper/BookingFormMapper.java
@@ -2,8 +2,10 @@ package com.berru.app.cabbookingapplication.mapper;
 
 import com.berru.app.cabbookingapplication.dto.BookingFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewBookingFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.UpdateBookingFormRequestDTO;
 import com.berru.app.cabbookingapplication.entity.BookingForm;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BookingFormMapper {
@@ -11,5 +13,7 @@ public interface BookingFormMapper {
     BookingForm toBookingForm(NewBookingFormRequestDTO newBookingFormRequestDTO);
 
     BookingFormResponseDTO toBookingFormResponseDTO(BookingForm bookingForm);
+
+    void updateBookingFormFromDTO(UpdateBookingFormRequestDTO updateBookingFormRequestDTO, @MappingTarget BookingForm bookingForm);
 
 }
