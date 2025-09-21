1871c3bc61e8efdd734b37a94f8b76bac67a0724
Berru Hanedar <berruhanedar@icloud.com>
2025-09-21 17:08:12 +0300

    extend BookingController with GET endpoints

--- src/main/java/com/berru/app/cabbookingapplication/controller/BookingController.java
+++ src/main/java/com/berru/app/cabbookingapplication/controller/BookingController.java
@@ -2,12 +2,13 @@ package com.berru.app.cabbookingapplication.controller;
 
 import com.berru.app.cabbookingapplication.dto.BookingFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewBookingFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
 import com.berru.app.cabbookingapplication.service.BookingServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -24,4 +25,19 @@ public class BookingController {
         BookingFormResponseDTO bookingFormResponseDTO = bookingServiceImpl.createBooking(newBookingFormRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookingFormResponseDTO);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookingFormResponseDTO> getByIdBooking(@PathVariable Integer id) {
+        BookingFormResponseDTO bookingFormResponseDTO = bookingServiceImpl.getAllBookings(id);
+        return ResponseEntity.ok(bookingFormResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<PaginationResponse<BookingFormResponseDTO>> listPaginated(
+            @RequestParam(defaultValue = "0") int pageNo,
+            @RequestParam(defaultValue = "10") int pageSize) {
+        PaginationResponse<BookingFormResponseDTO> paginationResponse = bookingServiceImpl.listPaginated(pageNo, pageSize);
+        return ResponseEntity.ok(paginationResponse);
+    }
+
 }
