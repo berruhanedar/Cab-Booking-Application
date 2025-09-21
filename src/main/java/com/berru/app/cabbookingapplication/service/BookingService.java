9d29ee4b6f75631464eadc34a1f78d6a3d88e201
Berru Hanedar <berruhanedar@icloud.com>
2025-09-21 17:09:01 +0300

    extend BookingService with getById and pagination

--- src/main/java/com/berru/app/cabbookingapplication/service/BookingService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/BookingService.java
@@ -2,7 +2,14 @@ package com.berru.app.cabbookingapplication.service;
 
 import com.berru.app.cabbookingapplication.dto.BookingFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewBookingFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
 
 public interface BookingService {
-   BookingFormResponseDTO createBooking(NewBookingFormRequestDTO newBookingFormRequestDTO);
+
+    BookingFormResponseDTO createBooking(NewBookingFormRequestDTO newBookingFormRequestDTO);
+
+    BookingFormResponseDTO getAllBookings(Integer id);
+
+    PaginationResponse<BookingFormResponseDTO> listPaginated(int pageNo, int pageSize);
+
 }
--- src/main/java/com/berru/app/cabbookingapplication/service/BookingServiceImpl.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/BookingServiceImpl.java
@@ -2,14 +2,22 @@ package com.berru.app.cabbookingapplication.service;
 
 import com.berru.app.cabbookingapplication.dto.BookingFormResponseDTO;
 import com.berru.app.cabbookingapplication.dto.NewBookingFormRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
 import com.berru.app.cabbookingapplication.entity.BookingForm;
+import com.berru.app.cabbookingapplication.exception.ResourceNotFoundException;
 import com.berru.app.cabbookingapplication.mapper.BookingFormMapper;
 import com.berru.app.cabbookingapplication.repository.BookingFormRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public class BookingServiceImpl implements BookingService{
+public class BookingServiceImpl implements BookingService {
 
     private final BookingFormRepository bookingFormRepository;
     private final BookingFormMapper bookingFormMapper;
@@ -21,9 +29,38 @@ public class BookingServiceImpl implements BookingService{
 
     @Override
     @Transactional
-    public BookingFormResponseDTO createBooking(NewBookingFormRequestDTO  newBookingFormRequestDTO) {
+    public BookingFormResponseDTO createBooking(NewBookingFormRequestDTO newBookingFormRequestDTO) {
         BookingForm bookingForm = bookingFormMapper.toBookingForm(newBookingFormRequestDTO);
         bookingFormRepository.save(bookingForm);
         return bookingFormMapper.toBookingFormResponseDTO(bookingForm);
+    }
+
+    @Override
+    @Transactional
+    public BookingFormResponseDTO getAllBookings(Integer id) {
+        BookingForm bookingForm = bookingFormRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Contact Form not found with id " + id));
+        return bookingFormMapper.toBookingFormResponseDTO(bookingForm);
+    }
+
+
+    @Override
+    @Transactional
+    public PaginationResponse<BookingFormResponseDTO> listPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+
+        Page<BookingForm> bookingFormPage = bookingFormRepository.findAll(pageable);
+
+        List<BookingFormResponseDTO> bookingFormResponseDTOList = bookingFormPage.getContent().stream()
+                .map(bookingFormMapper::toBookingFormResponseDTO)
+                .collect(Collectors.toList());
+
+        return PaginationResponse.<BookingFormResponseDTO>builder()
+                .content(bookingFormResponseDTOList)
+                .pageNo(bookingFormPage.getNumber())
+                .pageSize(bookingFormPage.getSize())
+                .totalElements(bookingFormPage.getTotalElements())
+                .isLast(bookingFormPage.isLast())
+                .build();
     }
 }
