92aac1d8007de721ab51086a660a499602e92105
Berru Hanedar <berruhanedar@icloud.com>
2025-09-23 20:37:16 +0300

    add JpaSpecificationExecutor to BookingFormRepository

--- src/main/java/com/berru/app/cabbookingapplication/repository/BookingFormRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/BookingFormRepository.java
@@ -2,6 +2,7 @@ package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.BookingForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookingFormRepository extends JpaRepository<BookingForm, Integer> {
+public interface BookingFormRepository extends JpaRepository<BookingForm, Integer>, JpaSpecificationExecutor<BookingForm> {
 }
