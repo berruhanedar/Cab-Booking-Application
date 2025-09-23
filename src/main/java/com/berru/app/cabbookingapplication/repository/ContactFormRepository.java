fda7481e797e3459ec0d8bfa4e299fa7b4f6014a
Berru Hanedar <berruhanedar@icloud.com>
2025-09-23 20:44:22 +0300

    extend ContactFormRepository with JpaSpecificationExecutor

--- src/main/java/com/berru/app/cabbookingapplication/repository/ContactFormRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/ContactFormRepository.java
@@ -2,11 +2,12 @@ package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.ContactForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ContactFormRepository extends JpaRepository<ContactForm, Integer> {
+public interface ContactFormRepository extends JpaRepository<ContactForm, Integer>, JpaSpecificationExecutor<ContactForm> {
     Optional<ContactForm> findByEmail(String email);
 }
