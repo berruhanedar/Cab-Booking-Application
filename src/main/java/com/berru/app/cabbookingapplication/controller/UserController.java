7a923c263a43c0282bd73852de15d77296a6ac71
Berru Hanedar <berruhanedar@icloud.com>
2025-09-25 17:20:06 +0300

    implement UserController methods as stubs

--- src/main/java/com/berru/app/cabbookingapplication/controller/UserController.java
+++ src/main/java/com/berru/app/cabbookingapplication/controller/UserController.java
@@ -1,10 +1,63 @@
 package com.berru.app.cabbookingapplication.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.berru.app.cabbookingapplication.dto.NewUserRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
+import com.berru.app.cabbookingapplication.dto.UpdateUserRequestDTO;
+import com.berru.app.cabbookingapplication.dto.UserResponseDTO;
+import com.berru.app.cabbookingapplication.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import javax.management.relation.RoleStatus;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponseDTO createUser(@RequestBody @Valid NewUserRequestDTO newUserRequestDTO) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDTO getUserById(@PathVariable Integer id) {
+        return null;
+    }
+
+    @GetMapping
+    public PaginationResponse<UserResponseDTO> listPaginated(
+            @RequestParam int pageNo,
+            @RequestParam int size) {
+        return null;
+    }
+
+    @GetMapping("/search")
+    public List<UserResponseDTO> searchUserByRsql(@RequestParam String query) {
+        return List.of();
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDTO updateUser(
+            @PathVariable Integer id,
+            @RequestBody UpdateUserRequestDTO updateUserRequestDTO) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Integer id) {
+    }
+
+    @PatchMapping("/{id}/role")
+    public UserResponseDTO changeUserRole(
+            @PathVariable Integer id,
+            @RequestParam RoleStatus newRole) {
+        return null;
+    }
 }
