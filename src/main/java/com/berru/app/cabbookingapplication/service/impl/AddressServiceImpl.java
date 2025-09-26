cfaba3495f2807ed154cd65e25d1d676e11b27f7
Berru Hanedar <berruhanedar@icloud.com>
2025-09-26 16:24:36 +0300

    implement listPaginated and searchAddressByRsql in AddressServiceImpl

--- src/main/java/com/berru/app/cabbookingapplication/service/impl/AddressServiceImpl.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/impl/AddressServiceImpl.java
@@ -7,11 +7,19 @@ import com.berru.app.cabbookingapplication.dto.UpdateAddressRequestDTO;
 import com.berru.app.cabbookingapplication.entity.Address;
 import com.berru.app.cabbookingapplication.mapper.AddressMapper;
 import com.berru.app.cabbookingapplication.repository.AddressRepository;
+import com.berru.app.cabbookingapplication.rsql.CustomRsqlVisitor;
 import com.berru.app.cabbookingapplication.service.AddressService;
+import cz.jirutka.rsql.parser.RSQLParser;
+import cz.jirutka.rsql.parser.ast.Node;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -25,16 +33,17 @@ public class AddressServiceImpl implements AddressService {
     }
 
 
-    @Transactional
     @Override
+    @Transactional
     public AddressResponseDTO saveAddress(NewAddressRequestDTO newAddressRequestDTO) {
         Address address = addressMapper.toAddress(newAddressRequestDTO);
         Address savedAddress = addressRepository.save(address);
         return addressMapper.toAddressDTO(savedAddress);
     }
 
-    @Transactional
+
     @Override
+    @Transactional
     public AddressResponseDTO getAddressById(Integer id) {
         Address address = addressRepository.findById(id).
                 orElseThrow(() -> new RuntimeException("Address not found"));
@@ -42,21 +51,50 @@ public class AddressServiceImpl implements AddressService {
     }
 
     @Override
+    @Transactional
     public PaginationResponse<AddressResponseDTO> listPaginated(int pageNo, int size) {
-        return null;
+        Pageable pageable = PageRequest.of(pageNo, size);
+
+        Page<Address> addressPage = addressRepository.findAll(pageable);
+
+        List<AddressResponseDTO> addressResponseDTOList = addressPage.getContent().stream()
+                .map(addressMapper::toAddressDTO)
+                .collect(Collectors.toList());
+
+        return PaginationResponse .<AddressResponseDTO>builder()
+                .content(addressResponseDTOList)
+                .pageNo(addressPage.getNumber())
+                .pageSize(addressPage.getSize())
+                .totalElements(addressPage.getTotalElements())
+                .totalPages(addressPage.getTotalPages())
+                .isLast(addressPage.isLast())
+                .build();
     }
 
     @Override
+    @Transactional
     public List<AddressResponseDTO> searchAddressByRsql(String query) {
-        return List.of();
+        RSQLParser parser = new RSQLParser();
+        Node rootNode = parser.parse(query);
+
+        CustomRsqlVisitor<Address> visitor = new CustomRsqlVisitor<>();
+        Specification<Address> spec = rootNode.accept(visitor);
+
+        List<Address> addresses = addressRepository.findAll(spec);
+
+        return addresses.stream()
+                .map(addressMapper::toAddressDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
+    @Transactional
     public AddressResponseDTO updateAddress(Integer id, UpdateAddressRequestDTO updateAddressRequestDTO) {
         return null;
     }
 
     @Override
+    @Transactional
     public void deleteAddressById(Integer id) {
 
     }
