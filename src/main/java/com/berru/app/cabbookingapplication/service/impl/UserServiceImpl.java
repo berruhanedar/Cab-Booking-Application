d91087fee2e56d75c1d9d94f9e382a0daede9ab1
Berru Hanedar <berruhanedar@icloud.com>
2025-09-28 22:16:15 +0300

    implement user creation with address association and email validation

--- src/main/java/com/berru/app/cabbookingapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/impl/UserServiceImpl.java
@@ -4,7 +4,12 @@ import com.berru.app.cabbookingapplication.dto.NewUserRequestDTO;
 import com.berru.app.cabbookingapplication.dto.PaginationResponse;
 import com.berru.app.cabbookingapplication.dto.UpdateUserRequestDTO;
 import com.berru.app.cabbookingapplication.dto.UserResponseDTO;
+import com.berru.app.cabbookingapplication.entity.Address;
+import com.berru.app.cabbookingapplication.entity.User;
+import com.berru.app.cabbookingapplication.exception.DuplicateEmailException;
+import com.berru.app.cabbookingapplication.exception.ResourceNotFoundException;
 import com.berru.app.cabbookingapplication.mapper.UserMapper;
+import com.berru.app.cabbookingapplication.repository.AddressRepository;
 import com.berru.app.cabbookingapplication.repository.UserRepository;
 import com.berru.app.cabbookingapplication.service.UserService;
 import org.springframework.stereotype.Service;
@@ -17,15 +22,28 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final AddressRepository addressRepository;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper,  AddressRepository addressRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.addressRepository = addressRepository;
     }
 
     @Override
     public UserResponseDTO createUser(NewUserRequestDTO newUserRequestDTO) {
-        return null;
+        User user = userMapper.toUser(newUserRequestDTO);
+
+        Address address = addressRepository.findById(newUserRequestDTO.getAddressId()).orElseThrow(()-> new ResourceNotFoundException("Address not found with id "+ newUserRequestDTO.getAddressId()));
+
+        validateNewUser(newUserRequestDTO);
+
+        address.setUser(user);
+        user.getAddresses().add(address);
+
+        User savedUser = userRepository.save(user);
+
+        return userMapper.toUserResponseDTO(savedUser);
     }
 
     @Override
@@ -50,11 +68,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUserById(Integer id) {
-
     }
 
     @Override
     public UserResponseDTO changeUserRole(Integer id, RoleStatus newRole) {
         return null;
+    }
+
+    private void validateNewUser(NewUserRequestDTO dto) {
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new DuplicateEmailException("Email already exists");
+        }
     }
 }
