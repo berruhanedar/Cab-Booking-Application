775edc0426031922021348fa80a9b0604666050a
Berru Hanedar <berruhanedar@icloud.com>
2025-10-04 20:47:12 +0300

    add VehicleService interface with CRUD and query methods

--- src/main/java/com/berru/app/cabbookingapplication/service/VehicleService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/VehicleService.java
@@ -1,4 +1,31 @@
 package com.berru.app.cabbookingapplication.service;
 
+import com.berru.app.cabbookingapplication.dto.*;
+import com.berru.app.cabbookingapplication.enums.VehicleEnergyType;
+import com.berru.app.cabbookingapplication.enums.VehicleType;
+import com.berru.app.cabbookingapplication.enums.VehicleStatus;
+
 public interface VehicleService {
+
+    VehicleResponseDTO createVehicle(NewVehicleRequestDTO newVehicleRequestDTO);
+
+    VehicleResponseDTO getVehicleById(Integer id);
+
+    VehicleResponseDTO getVehicleByPlate(String plate);
+
+    PaginationResponse<VehicleResponseDTO> listPaginated(int pageNo, int size);
+
+    PaginationResponse<VehicleResponseDTO> listVehiclesByType(VehicleType type, int pageNo, int size);
+
+    PaginationResponse<VehicleResponseDTO> listVehiclesByEnergy(VehicleEnergyType energyType, int pageNo, int size);
+
+    PaginationResponse<VehicleResponseDTO> listVehiclesByStatus(VehicleStatus status, int pageNo, int size);
+
+    PaginationResponse<VehicleResponseDTO> searchVehicleByRsql(String query, int pageNo, int size);
+
+    VehicleResponseDTO updateVehicle(Integer id, UpdateVehicleRequestDTO updateVehicleRequestDTO);
+
+    VehicleResponseDTO updateVehicleStatus(Integer id, VehicleStatus status);
+
+    void deleteVehicleById(Integer id);
 }
