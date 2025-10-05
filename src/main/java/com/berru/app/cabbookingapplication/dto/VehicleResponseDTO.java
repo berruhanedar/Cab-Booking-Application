3e872b30d24c3dc250931723e34aca493a0eb37e
Berru Hanedar <berruhanedar@icloud.com>
2025-10-05 19:19:25 +0300

    add JsonInclude to VehicleResponseDTO

--- src/main/java/com/berru/app/cabbookingapplication/dto/VehicleResponseDTO.java
+++ src/main/java/com/berru/app/cabbookingapplication/dto/VehicleResponseDTO.java
@@ -3,9 +3,11 @@ package com.berru.app.cabbookingapplication.dto;
 import com.berru.app.cabbookingapplication.enums.VehicleEnergyType;
 import com.berru.app.cabbookingapplication.enums.VehicleStatus;
 import com.berru.app.cabbookingapplication.enums.VehicleType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class VehicleResponseDTO {
 
     private String plate;
