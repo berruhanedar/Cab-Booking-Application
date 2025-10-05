1e3887c6e6f33c8ef6de4fdd49c54b41d2573ff8
Berru Hanedar <berruhanedar@icloud.com>
2025-10-06 02:03:50 +0300

    add VehicleController with create and getVehicleById endpoints

--- src/main/java/com/berru/app/cabbookingapplication/controller/VehicleController.java
+++ src/main/java/com/berru/app/cabbookingapplication/controller/VehicleController.java
@@ -6,10 +6,7 @@ import com.berru.app.cabbookingapplication.service.VehicleService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/vehicle")
@@ -27,6 +24,12 @@ public class VehicleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(vehicleResponseDTO);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<VehicleResponseDTO> getVehicleById(@PathVariable Integer id) {
+        VehicleResponseDTO vehicleResponseDTO = vehicleService.getVehicleById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleResponseDTO);
+
+    }
 
 
 }
