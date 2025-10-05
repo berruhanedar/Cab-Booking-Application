e4927658f2dbd5d19c43b0f251109d987cd516db
Berru Hanedar <berruhanedar@icloud.com>
2025-10-06 02:06:46 +0300

    extended VehicleRepository with a new query method findByPlate()

--- src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer>, JpaSpecificationExecutor<Vehicle> {
+    Optional<Vehicle> findByPlate(String plate);
 }
