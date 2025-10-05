d066fabfd991fb327336f78929840672dfb66f48
Berru Hanedar <berruhanedar@icloud.com>
2025-10-05 19:20:40 +0300

    implement VehicleServiceImpl skeleton with createVehicle

---
+++ src/main/java/com/berru/app/cabbookingapplication/service/impl/VehicleServiceImpl.java
@@ -0,0 +1,85 @@
+package com.berru.app.cabbookingapplication.service.impl;
+
+import com.berru.app.cabbookingapplication.dto.NewVehicleRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaginationResponse;
+import com.berru.app.cabbookingapplication.dto.UpdateVehicleRequestDTO;
+import com.berru.app.cabbookingapplication.dto.VehicleResponseDTO;
+import com.berru.app.cabbookingapplication.entity.Vehicle;
+import com.berru.app.cabbookingapplication.enums.VehicleEnergyType;
+import com.berru.app.cabbookingapplication.enums.VehicleStatus;
+import com.berru.app.cabbookingapplication.enums.VehicleType;
+import com.berru.app.cabbookingapplication.mapper.VehicleMapper;
+import com.berru.app.cabbookingapplication.repository.VehicleRepository;
+import com.berru.app.cabbookingapplication.service.VehicleService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleServiceImpl implements VehicleService {
+
+   private final VehicleRepository vehicleRepository;
+   private final VehicleMapper vehicleMapper;
+
+   public VehicleServiceImpl(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper) {
+       this.vehicleRepository = vehicleRepository;
+       this.vehicleMapper = vehicleMapper;
+   }
+
+    @Override
+    @Transactional
+    public VehicleResponseDTO createVehicle(NewVehicleRequestDTO newVehicleRequestDTO) {
+        Vehicle vehicle = vehicleMapper.toVehicle(newVehicleRequestDTO);
+        Vehicle savedVehicle = vehicleRepository.save(vehicle);
+        return vehicleMapper.toVehicleResponseDTO(savedVehicle);
+    }
+
+    @Override
+    public VehicleResponseDTO getVehicleById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public VehicleResponseDTO getVehicleByPlate(String plate) {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<VehicleResponseDTO> listPaginated(int pageNo, int size) {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<VehicleResponseDTO> listVehiclesByType(VehicleType type, int pageNo, int size) {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<VehicleResponseDTO> listVehiclesByEnergy(VehicleEnergyType energyType, int pageNo, int size) {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<VehicleResponseDTO> listVehiclesByStatus(VehicleStatus status, int pageNo, int size) {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<VehicleResponseDTO> searchVehicleByRsql(String query, int pageNo, int size) {
+        return null;
+    }
+
+    @Override
+    public VehicleResponseDTO updateVehicle(Integer id, UpdateVehicleRequestDTO updateVehicleRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public VehicleResponseDTO updateVehicleStatus(Integer id, VehicleStatus status) {
+        return null;
+    }
+
+    @Override
+    public void deleteVehicleById(Integer id) {
+
+    }
+}
