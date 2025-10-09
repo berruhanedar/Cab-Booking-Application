a5ddb6335e01b66be634100c0e27a43624573a05
Berru Hanedar <berruhanedar@icloud.com>
2025-10-09 16:28:08 +0300

    extend VehicleRepository with methods to find vehicles by energy type and status

--- src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
@@ -1,6 +1,8 @@
 package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.Vehicle;
+import com.berru.app.cabbookingapplication.enums.VehicleEnergyType;
+import com.berru.app.cabbookingapplication.enums.VehicleStatus;
 import com.berru.app.cabbookingapplication.enums.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -14,5 +16,9 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Integer>, JpaS
     Optional<Vehicle> findByPlate(String plate);
 
     List<Vehicle> findByType(VehicleType type);
+
+    List<Vehicle> findByEnergy(VehicleEnergyType energyType);
+
+    List<Vehicle> findyStatus (VehicleStatus  status);
 
 }
