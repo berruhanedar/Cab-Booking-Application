fb09c61685c26f6c787eaa57300ccef1fb56c0aa
Berru Hanedar <berruhanedar@icloud.com>
2025-10-10 01:29:29 +0300

    update VehicleMapper to use UpdateVehicleRequestDTO instead of NewVehicleRequestDTO in update method

--- src/main/java/com/berru/app/cabbookingapplication/mapper/VehicleMapper.java
+++ src/main/java/com/berru/app/cabbookingapplication/mapper/VehicleMapper.java
@@ -1,6 +1,7 @@
 package com.berru.app.cabbookingapplication.mapper;
 
 import com.berru.app.cabbookingapplication.dto.NewVehicleRequestDTO;
+import com.berru.app.cabbookingapplication.dto.UpdateVehicleRequestDTO;
 import com.berru.app.cabbookingapplication.dto.VehicleResponseDTO;
 import com.berru.app.cabbookingapplication.entity.Vehicle;
 import org.mapstruct.Mapper;
@@ -13,6 +14,6 @@ public interface VehicleMapper {
 
     VehicleResponseDTO toVehicleResponseDTO(Vehicle vehicle);
 
-    void updateVehicleDTO(@MappingTarget NewVehicleRequestDTO newVehicleRequestDTO, Vehicle vehicle);
+    void updateVehicleDTO(@MappingTarget UpdateVehicleRequestDTO updateVehicleRequestDTO, Vehicle vehicle);
 
 }
