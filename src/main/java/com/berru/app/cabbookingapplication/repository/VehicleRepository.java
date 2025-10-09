ef0a8bf9eeb57f5e06fdf3c599e6fcc35d4ef216
Berru Hanedar <berruhanedar@icloud.com>
2025-10-09 16:14:51 +0300

    add findByType method to VehicleRepository for querying vehicles by type

--- src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/VehicleRepository.java
@@ -1,13 +1,18 @@
 package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.Vehicle;
+import com.berru.app.cabbookingapplication.enums.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer>, JpaSpecificationExecutor<Vehicle> {
     Optional<Vehicle> findByPlate(String plate);
+
+    List<Vehicle> findByType(VehicleType type);
+
 }
