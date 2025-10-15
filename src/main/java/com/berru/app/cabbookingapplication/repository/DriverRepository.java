002315df51e2d23215ba5a1541ec10e46ea64e4f
Berru Hanedar <berruhanedar@icloud.com>
2025-10-15 20:09:35 +0300

    enables service layer to fetch driver profiles directly using user ID

--- src/main/java/com/berru/app/cabbookingapplication/repository/DriverRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/DriverRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Integer>, JpaSpecificationExecutor<Driver> {
     boolean existsByUserId(Integer userId);
+    Optional<Driver> findByUserId(Integer userId);
 }
