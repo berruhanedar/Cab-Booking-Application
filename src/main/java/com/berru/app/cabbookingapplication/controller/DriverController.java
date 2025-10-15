ef2125a6bd74a00d05f2fad63e0bc814fa768bbb
Berru Hanedar <berruhanedar@icloud.com>
2025-10-15 20:01:45 +0300

    integrated service layer call to getDriverById

--- src/main/java/com/berru/app/cabbookingapplication/controller/DriverController.java
+++ src/main/java/com/berru/app/cabbookingapplication/controller/DriverController.java
@@ -6,10 +6,7 @@ import com.berru.app.cabbookingapplication.service.DriverService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/driver")
@@ -22,12 +19,16 @@ public class DriverController {
     }
 
     @PostMapping
-    public ResponseEntity<DriverResponseDTO> createDriver(@RequestBody @Valid NewDriverRequestDTO newDriverRequestDTO){
+    public ResponseEntity<DriverResponseDTO> createDriver(@RequestBody @Valid NewDriverRequestDTO newDriverRequestDTO) {
         DriverResponseDTO driverResponseDTO = driverService.createDriver(newDriverRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(driverResponseDTO);
     }
 
-
+    @GetMapping("/id/{id}")
+    public ResponseEntity<DriverResponseDTO> getDriverById(@PathVariable Integer id) {
+        DriverResponseDTO driverResponseDTO = driverService.getDriverById(id);
+        return ResponseEntity.ok().body(driverResponseDTO);
+    }
 
 
 }
