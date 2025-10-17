5b937e4e8205eb132339a095b56d1e38e3c245a7
Berru Hanedar <berruhanedar@icloud.com>
2025-10-17 07:07:58 +0300

    ignore vehicles mapping in DriverMapper and add VehicleMapper usage

--- src/main/java/com/berru/app/cabbookingapplication/mapper/DriverMapper.java
+++ src/main/java/com/berru/app/cabbookingapplication/mapper/DriverMapper.java
@@ -5,13 +5,15 @@ import com.berru.app.cabbookingapplication.dto.NewDriverRequestDTO;
 import com.berru.app.cabbookingapplication.dto.UpdateDriverRequestDTO;
 import com.berru.app.cabbookingapplication.entity.Driver;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {VehicleMapper.class})
 public interface DriverMapper {
 
     DriverResponseDTO toDriverResponseDTO(Driver driver);
 
+    @Mapping(target = "vehicles", ignore = true)
     Driver toDriver(NewDriverRequestDTO newDriverRequestDTO);
 
     void updateDriverFromDTO(UpdateDriverRequestDTO dto, @MappingTarget Driver driver);
