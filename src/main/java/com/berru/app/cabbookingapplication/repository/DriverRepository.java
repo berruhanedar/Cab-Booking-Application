490902cd0ad2e28fd97e8e0feb53614cd3460316
Berru Hanedar <berruhanedar@icloud.com>
2025-10-17 05:06:50 +0300

    add method to find drivers by availability

--- src/main/java/com/berru/app/cabbookingapplication/repository/DriverRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/DriverRepository.java
@@ -1,6 +1,7 @@
 package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.Driver;
+import com.berru.app.cabbookingapplication.enums.DriverAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,13 @@ import java.util.Optional;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Integer>, JpaSpecificationExecutor<Driver> {
+
     boolean existsByUserId(Integer userId);
+
     Optional<Driver> findByUserId(Integer userId);
+
     List<Driver> findByRatingGreaterThanEqual(Double rating);
+
+    List<Driver> findByAvailability(DriverAvailability availability);
+
 }
