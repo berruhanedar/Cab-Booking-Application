63ae032e37d963f4a9df86f714dc9187def76833
Berru Hanedar <berruhanedar@icloud.com>
2025-10-28 20:08:25 +0300

    add updateRating endpoint to RatingController

--- src/main/java/com/berru/app/cabbookingapplication/controller/RatingController.java
+++ src/main/java/com/berru/app/cabbookingapplication/controller/RatingController.java
@@ -2,14 +2,12 @@ package com.berru.app.cabbookingapplication.controller;
 
 import com.berru.app.cabbookingapplication.dto.NewRatingRequestDTO;
 import com.berru.app.cabbookingapplication.dto.RatingResponseDTO;
+import com.berru.app.cabbookingapplication.dto.UpdateRatingRequestDTO;
 import com.berru.app.cabbookingapplication.service.RatingService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/rating")
@@ -26,4 +24,12 @@ public class RatingController {
         RatingResponseDTO ratingResponseDTO = ratingService.createRating(newRatingRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingResponseDTO);
     }
+
+    @PutMapping("/update/{ratingId}")
+    public ResponseEntity<RatingResponseDTO> updateRating(@PathVariable Integer ratingId, @RequestBody @Valid UpdateRatingRequestDTO updateRatingRequestDTO) {
+        RatingResponseDTO ratingResponseDTO = ratingService.updateRating(ratingId, updateRatingRequestDTO);
+        return ResponseEntity.ok(ratingResponseDTO);
+    }
+
+
 }
