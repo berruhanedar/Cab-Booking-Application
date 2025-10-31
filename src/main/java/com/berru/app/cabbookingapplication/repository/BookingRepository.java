321c97073e36ba02246aed7cd5c8b445acc6de38
Berru Hanedar <berruhanedar@icloud.com>
2025-11-01 00:06:25 +0300

    add custom query methods to BookingRepository for driver and status checks

--- src/main/java/com/berru/app/cabbookingapplication/repository/BookingRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/BookingRepository.java
@@ -1,8 +1,19 @@
 package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.Booking;
+import com.berru.app.cabbookingapplication.entity.Driver;
+import com.berru.app.cabbookingapplication.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer>, JpaSpecificationExecutor<Booking> {
+    boolean existsByDriverAndDateAndTimeAndStatusNot(
+            Driver driver, LocalDate date, LocalTime time, BookingStatus status);
+
+    List<Booking> findByStatus(BookingStatus status);
+
 }
