22323fe6fd1e18af1c52e200666ac039b6f4f6bf
Berru Hanedar <berruhanedar@icloud.com>
2025-11-04 00:38:23 +0300

    add @Repository annotation

--- src/main/java/com/berru/app/cabbookingapplication/repository/BookingRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/BookingRepository.java
@@ -5,11 +5,13 @@ import com.berru.app.cabbookingapplication.entity.Driver;
 import com.berru.app.cabbookingapplication.enums.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer>, JpaSpecificationExecutor<Booking> {
 
     boolean existsByDriverAndDateAndTimeAndStatusNot(
