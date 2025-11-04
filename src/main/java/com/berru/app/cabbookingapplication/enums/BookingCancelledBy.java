ff0a13a893e872fd7bdf4fc5a4e034b4b2793f2a
Berru Hanedar <berruhanedar@icloud.com>
2025-11-05 02:59:38 +0300

    renamed

--- src/main/java/com/berru/app/cabbookingapplication/enums/BookingCancelledBy.java
+++
@@ -1,5 +0,0 @@
-package com.berru.app.cabbookingapplication.enums;
-
-public enum BookingCancelledBy {
-    CUSTOMER, DRIVER
-}
---
+++ src/main/java/com/berru/app/cabbookingapplication/enums/CancelledBy.java
@@ -0,0 +1,5 @@
+package com.berru.app.cabbookingapplication.enums;
+
+public enum CancelledBy {
+    CUSTOMER, DRIVER, ADMIN, SYSTEM
+}
