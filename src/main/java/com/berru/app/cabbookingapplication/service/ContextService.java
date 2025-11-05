31a5b7e946d983bdd6a95382068bd076c69cd75c
Berru Hanedar <berruhanedar@icloud.com>
2025-11-06 01:44:15 +0300

    introduce ContextService interface for retrieving current user and role details

--- src/main/java/com/berru/app/cabbookingapplication/service/ContextService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/ContextService.java
@@ -1,4 +1,13 @@
 package com.berru.app.cabbookingapplication.service;
 
+/* Amaç: Sistemde o an oturum açmış kullanıcıyı veya rolünü almak.
+Spring Security kullanıyorsan SecurityContextHolder’dan wrapper olabilir.
+ */
 public interface ContextService {
+
+    Integer getCurrentUserId();
+
+    String getCurrentUserEmail();
+
+    boolean hasRole(String roleName);
 }
