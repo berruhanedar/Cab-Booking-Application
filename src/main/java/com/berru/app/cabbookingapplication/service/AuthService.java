194c92f09a6a4b6e39de305dcd99178649e0442d
Berru Hanedar <berruhanedar@icloud.com>
2025-11-06 01:43:51 +0300

    define AuthService interface with authentication-related methods

--- src/main/java/com/berru/app/cabbookingapplication/service/AuthService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/AuthService.java
@@ -1,4 +1,16 @@
 package com.berru.app.cabbookingapplication.service;
 
+import com.berru.app.cabbookingapplication.dto.AuthResponseDTO;
+import com.berru.app.cabbookingapplication.dto.LoginRequestDTO;
+import com.berru.app.cabbookingapplication.dto.NewUserRequestDTO;
+
 public interface AuthService {
+
+    AuthResponseDTO register(NewUserRequestDTO newUserRequestDTO);
+
+    AuthResponseDTO login(LoginRequestDTO loginRequestDTO);
+
+    void logout(String token);
+
+    AuthResponseDTO refreshToken(String refreshToken);
 }
