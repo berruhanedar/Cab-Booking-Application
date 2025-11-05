ec7f24d73d953411a687719bc8273f7eba0e19aa
Berru Hanedar <berruhanedar@icloud.com>
2025-11-06 01:47:03 +0300

    define PaymentService interface with methods

--- src/main/java/com/berru/app/cabbookingapplication/service/PaymentService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/PaymentService.java
@@ -1,4 +1,13 @@
 package com.berru.app.cabbookingapplication.service;
 
+import com.berru.app.cabbookingapplication.dto.NewPaymentRequestDTO;
+import com.berru.app.cabbookingapplication.dto.PaymentResponseDTO;
+
 public interface PaymentService {
+
+    PaymentResponseDTO processPayment(NewPaymentRequestDTO newPaymentRequestDTO);
+
+    PaymentResponseDTO refundPayment(Integer bookingId);
+
+    PaymentResponseDTO getPaymentDetails(Integer paymentId);
 }
