c6e490bf5007e3e865917e876aab74d96bf19d13
Berru Hanedar <berruhanedar@icloud.com>
2025-11-07 02:46:57 +0300

    set default value for PaymentType enum to RIDE_FEE

--- src/main/java/com/berru/app/cabbookingapplication/enums/PaymentType.java
+++ src/main/java/com/berru/app/cabbookingapplication/enums/PaymentType.java
@@ -1,5 +1,7 @@
 package com.berru.app.cabbookingapplication.enums;
 
 public enum PaymentType {
-    RIDE_FEE, TIP, REFUND
+    RIDE_FEE, TIP, REFUND;
+
+    public static final PaymentType DEFAULT = RIDE_FEE;
 }
