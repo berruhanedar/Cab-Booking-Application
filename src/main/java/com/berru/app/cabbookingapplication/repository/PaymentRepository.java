1b088cee7ad80fc7543ffae8ab2f4f4459ed3ab3
Berru Hanedar <berruhanedar@icloud.com>
2025-11-13 18:18:30 +0300

    add findByBookingId method to PaymentRepository for refund operations

--- src/main/java/com/berru/app/cabbookingapplication/repository/PaymentRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/PaymentRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment,Integer>, JpaSpecificationExecutor<Payment> {
+    Optional<Payment> findByBookingId(Integer bookingId);
 }
