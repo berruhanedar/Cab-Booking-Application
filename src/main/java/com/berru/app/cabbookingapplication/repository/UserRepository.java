bf066893fae31ee8c3b7961e346e658977ea1a93
Berru Hanedar <berruhanedar@icloud.com>
2025-11-15 17:51:01 +0300

    add findByEmailAndStatus method to UserRepository

--- src/main/java/com/berru/app/cabbookingapplication/repository/UserRepository.java
+++ src/main/java/com/berru/app/cabbookingapplication/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.berru.app.cabbookingapplication.repository;
 
 import com.berru.app.cabbookingapplication.entity.User;
+import com.berru.app.cabbookingapplication.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> , JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmailAndStatus(String email, UserStatus status);
 }
