6295ec2608115b07b393f22d2af2e943682ae932
Berru Hanedar <berruhanedar@icloud.com>
2025-11-22 21:08:16 +0300

    implement ContextService with Spring Security to get current user info and roles

--- src/main/java/com/berru/app/cabbookingapplication/service/ContextService.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/ContextService.java
@@ -1,8 +1,5 @@
 package com.berru.app.cabbookingapplication.service;
 
-/* Amaç: Sistemde o an oturum açmış kullanıcıyı veya rolünü almak.
-Spring Security kullanıyorsan SecurityContextHolder’dan wrapper olabilir.
- */
 public interface ContextService {
 
     Integer getCurrentUserId();
--- src/main/java/com/berru/app/cabbookingapplication/service/impl/ContextServiceImpl.java
+++ src/main/java/com/berru/app/cabbookingapplication/service/impl/ContextServiceImpl.java
@@ -1,20 +1,45 @@
 package com.berru.app.cabbookingapplication.service.impl;
 
+import com.berru.app.cabbookingapplication.config.security.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import com.berru.app.cabbookingapplication.service.ContextService;
 
+@Service
 public class ContextServiceImpl implements ContextService {
+
     @Override
     public Integer getCurrentUserId() {
-        return 0;
+        CustomUserDetails userDetails = getCurrentUserDetails();
+        return (userDetails != null) ? userDetails.getUser().getId() : null;
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return "";
+        CustomUserDetails userDetails = getCurrentUserDetails();
+        return (userDetails != null) ? userDetails.getUser().getEmail() : null;
     }
 
     @Override
     public boolean hasRole(String roleName) {
+        CustomUserDetails userDetails = getCurrentUserDetails();
+        if (userDetails != null) {
+            for (GrantedAuthority authority : userDetails.getAuthorities()) {
+                if (authority.getAuthority().equals("ROLE_" + roleName)) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+
+    private CustomUserDetails getCurrentUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
+            return (CustomUserDetails) authentication.getPrincipal();
+        }
+        return null;
     }
 }
